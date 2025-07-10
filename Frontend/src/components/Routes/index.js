b4e5e944c7ca1763c43a59084071223cb652cded
Tanisha Maheshwari <maheshwaritanisha3802@gmail.com>
2025-07-11 01:27:23 +0530

    Added Personalized Practice Roadmap UI for Students

--- Frontend/src/components/Routes/index.js
+++ Frontend/src/components/Routes/index.js
@@ -4,8 +4,12 @@ import SignUpArchit from "../Institute/SignUp-Archit.vue";
 import SignUp from "../Student/SignUp-Tanuja.vue";
 import AdminHome from "../Admin/AdminDashboard.vue";
 import StudentHome from "../Student/StudentHome.vue";
+<<<<<<< Updated upstream
 import StudentPractice from "../Student/StudentPractice.vue";
 import StudentProfile from "../Student/StudentProfile.vue";
+=======
+import StudentPracticeContent from "../Student/PracticeContent.vue";
+>>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,9 +19,13 @@ const router = createRouter({
     {path: "/student" , component:SignUp},
     {path: "/admin-home" , component: AdminHome},
     {path: "/student-home" , component: StudentHome},
+<<<<<<< Updated upstream
     {path: "/student-practice" , component: StudentPractice},
 
     {path : "/studentprofile" , component: StudentProfile},
+=======
+    {path: "/student-practice-content" , component: StudentPracticeContent},
+>>>>>>> Stashed changes
   ]
 })
 
