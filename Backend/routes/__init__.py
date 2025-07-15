139d8488d97bb923c7f7366e844601ddeafb1e81
Hitesh-Binjrawat <nnnhkb@gmail.com>
2025-07-15 16:56:07 +0530

    Updated __init__ files

--- Backend/routes/__init__.py
+++ Backend/routes/__init__.py
@@ -1,18 +1,14 @@
 from flask_restful import Api
-from .user import UserApi
+from .user import UserApi,VerifyStudents
 from .log_in_out import LoginResource,LogoutResource
-from .institute_resource import InstituteResource,AllInstitute,ToggleBlockInstitute
+from .institute_resource import InstituteResource,AllInstitute,ToggleBlockInstitute,InstituteTeacher
 from .teachers_resource import TeacherResource,UserTeacherResource,TeacherWiseProgress
 from .unit_resource import CourseResource,CompletedCourse,CourseProgress
 from .lectures import LectureResource, UserLectureResource
 from .questions_resource import QuestionResource
-<<<<<<< HEAD
 from .ai_features import RecommendedCourses
-
-=======
 from .questions_resource import AllQuestionResource
 from .streak_resource import StreakResource,StreakCalendarData
->>>>>>> b4e5e944c7ca1763c43a59084071223cb652cded
 
 
 def init_routes(app):
@@ -28,18 +24,16 @@ def init_routes(app):
     api.add_resource(LectureResource, '/lecture', '/lecture/<int:id>')
     api.add_resource(AllInstitute, '/all_institute')
     api.add_resource(QuestionResource, '/question','/question/<int:id>')
-<<<<<<< HEAD
     api.add_resource(ToggleBlockInstitute, '/toggle_block_institute/<int:institute_id>')
     api.add_resource(CompletedCourse, '/user_course_completed/<int:user_id>')
     api.add_resource(UserLectureResource, '/user_lecture_watched/<int:user_id>/<int:lecture_id>')
     api.add_resource(CourseProgress, '/course_progress/<int:user_id>')
     api.add_resource(UserTeacherResource,"/user_teacher/<int:user_id>")
     api.add_resource(TeacherWiseProgress,"/teacher_wise_progress/<int:institute_id>")
-    api.add_resource(RecommendedCourses,"/recommended_courses/<int:user_id>")
-
-=======
+    api.add_resource(RecommendedCourses,"/recommended_courses/<int:user_id>") 
     api.add_resource(AllQuestionResource, '/questions/unit/<int:unit_id>')
     api.add_resource(StreakResource,'/user/streak')
     api.add_resource(StreakCalendarData,'/user/streak/calendar')
->>>>>>> b4e5e944c7ca1763c43a59084071223cb652cded
+    api.add_resource(VerifyStudents,"/unverified_students/<int:institute_id>","/verify_student/<int:user_id>")
+    api.add_resource(InstituteTeacher,"/institute_wise_teachers/<int:institute_id>")
 
