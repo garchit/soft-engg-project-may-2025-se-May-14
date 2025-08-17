3b8c32be6fba95e33eae2600a0f14c4e6eeee1bb
Tanuu <tanujanair@Tanuus-MacBook-Air.local>
2025-08-17 20:52:39 +0530

    fixed celery worker

--- Backend/celery_worker.py
+++ Backend/celery_worker.py
@@ -1,37 +1,47 @@
 from celery import Celery
-from datetime import timedelta
+from celery.schedules import crontab
 from factory import create_app
 from config import Config
 
-# Create the Flask app
+
 flask_app = create_app()
 
-# Create the Celery app
+
 celery = Celery(
     flask_app.import_name,
     broker=Config.CELERY_BROKER_URL,
     backend=Config.CELERY_RESULT_BACKEND,
-    include=["tasks"]
+    include=["tasks"]  
 )
 
-# Load Flask config into Celery
 celery.conf.update(flask_app.config)
 
-# Ensure tasks run inside Flask app context
+
 class ContextTask(celery.Task):
     def __call__(self, *args, **kwargs):
         with flask_app.app_context():
             return self.run(*args, **kwargs)
 
+
 celery.Task = ContextTask
 
-# Celery Beat schedule for periodic tasks
+
 celery.conf.beat_schedule = {
-    # Send class reports every 30 seconds
-    'class-report-every-30-sec': {
+    'send-monthly-teacher-reports': {
         'task': 'tasks.send_monthly_class_reports_to_teachers',
-        'schedule': timedelta(seconds=30),
+
+        'schedule': crontab(minute='*/2'), 
+
+        # 'schedule': timedelta(days=30),
     },
+    'schedule-weekly-parent-reports': {
+        'task': 'tasks.schedule_all_parent_reports',
+
+        'schedule': crontab(minute='*/2'),
+
+        # 'schedule': timedelta(weeks=1),
+    }
 }
+
 
 celery.conf.timezone = 'Asia/Kolkata'
